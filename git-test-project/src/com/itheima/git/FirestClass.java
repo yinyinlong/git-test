73338e916608abf2325dd96fef888ac16d17b8f2
yinyinlong <yinyinlong@itcast.cn>
2019-06-24 18:39:32 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirestClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirestClass {
+    public static void main ( String[] args ) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
